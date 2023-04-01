7454df2fdfb3713057a1948a37a4342ef1887726
Bijaya Panday <76951767+Bijay434@users.noreply.github.com>
2023-04-01 20:55:39 +0545

    Add files via upload

--- Day_4_project_rock_paper_sissor.py
+++ Day_4_project_rock_paper_sissor.py
@@ -36,22 +36,31 @@ computer = random.randint(0,3)
 player = input("Choose: 'Rock', 'Paper', 'Scissors' ").lower()
 if player == 'rock':
     if computer == 0:
+        print(rock)
         print("It's a draw!")
     elif computer == 1:
+        print(paper)
         print("Paper wins against rock. Computer wins!")
     elif computer == 2:
+        print(scissors)
         print("Rock wins against scissors. Player wins!")
 elif player == 'paper':
     if computer == 1:
+        print(paper)
         print("It's a draw!")
     elif computer == 2:
+        print(scissors)
         print("Scissors wins against paper. Computer wins!")
     elif computer == 0:
+        print(rock)
         print("Paper wins against rock. Player wins!")
 elif player == 'scissors':
     if computer == 2:
+        print(scissors)
         print("It's a draw!")
     elif computer == 0:
+        print(rock)
         print("Rock wins against scissors. Computer wins!")
     elif computer == 1:
+        print(paper)
         print("Scissors wins against paper. Player wins!")
