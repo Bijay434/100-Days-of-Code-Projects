73fc31c9cdfbb9338724b3a4643269136134450c
Bijaya Panday <76951767+Bijay434@users.noreply.github.com>
2023-04-01 11:10:50 +0545

    Add files via upload

---
+++ Day_1_band_name_generator.py
@@ -0,0 +1,9 @@
+# Generate a band name using the city you are born and the name of your pet.
+print("Welcome to the band name generator for your band!")
+
+city = input("What is the name of the city you were born? ").title()
+pet = input("What name you have given for your pet? ").title()
+
+print()
+print(f"Your band name could be: '{city} {pet}'")
+print('Congratulation!! You got an awesome band name.')
